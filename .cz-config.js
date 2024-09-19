c385aeb29bd382832a5777b5ac7e5b831dc9d3f4
Ricardo Soto <ricardosoto.saez@gmail.com>
2024-09-19 00:52:55 -0300

    chore: configura commitizen y standard version

---
+++ .cz-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    types: [
+      { value: 'feat', name: 'feat:     A new feature' },
+      { value: 'fix', name: 'fix:      A bug fix' },
+      { value: 'chore', name: 'chore:      Other changes' },
+      // otros tipos...
+    ],
+  };
